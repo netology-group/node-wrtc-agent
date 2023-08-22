b0e1f7279d9817fe902053f90e804c1aa2ee1cf1
Aleksey Konstantinov <a.konstantinov@netology-group.ru>
2023-08-22 13:35:35 +0300

    Added http client for conference

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:netology-group/node-wrtc-agent.git
-cd node-wrtc-agent
+#git clone git@github.com:foxford/node-wrtc-agent.git
+#cd node-wrtc-agent
+node -v
+npm -v
+nvm -v
+#nvm install v16.15
+#nvm use
+npm install -g node-pre-gyp
 npm i
-npm run copy-module-linux
-sudo npm link
+npm link
