59115f26489a8d1399d44d60a53c885ac6eec785
Aleksey Konstantinov <a.konstantinov@foxford.ru>
2023-08-30 23:25:04 +0300

    Removed nvm from scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-#git clone git@github.com:foxford/node-wrtc-agent.git
-#cd node-wrtc-agent
 node -v
 npm -v
-nvm -v
-#nvm install v16.15
-#nvm use
-npm install -g node-pre-gyp
-npm i
+npm ci
 npm link
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,6 @@
 sudo apt update
 sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
 curl -sL https://deb.nodesource.com/setup_16.x | sudo bash
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 sudo apt update
 sudo apt -y install gcc g++ make
 sudo apt -y install nodejs
